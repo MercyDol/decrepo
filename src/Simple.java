d4560349d324615fa9fd01b12701094c84262a4f
Mercy <mercy.dol@capgemini.com>
2024-12-17 16:06:54 +0530

    third commit

--- src/Simple.java
+++ src/Simple.java
@@ -4,7 +4,7 @@ public class Simple {
 	public static void main(String[] args) {
 	System.out.println("Message from STS");
 	System.out.println("Message from GITHUB");
-
+	System.out.println("Message from STS ");
 	}
 
 }
